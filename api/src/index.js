988123d7ab455f0dc9c5267a5c11c3ab0a064ec6
Rishi Anand <rishi.a@honeysys.com>
2023-09-27 17:59:16 +0530

    express json middleware added

--- api/src/index.js
+++ api/src/index.js
@@ -11,6 +11,7 @@ const taskController = require("./controller/task.controller")
 
 
 //MIDDLEWARES
+app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
