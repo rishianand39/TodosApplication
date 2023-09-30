3c00856e885386659d4967a1a6b37dc84965e788
rishianand39 <rishi.rn818@gmail.com>
2023-09-30 18:49:53 +0530

    chip designed and added functionality

--- client/src/components/addTaskModal.jsx
+++ client/src/components/addTaskModal.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/scss/addTaskModal.scss";
 import CloseIcon from "@mui/icons-material/Close";
 import MinimizeIcon from "@mui/icons-material/Minimize";
+import Chip from "../building-block/chip";
 
 const AddTaskModal = ({ isOpen, closeModal }) => {
+  const [tags, setTags] = useState([]);
+  const [tagInput, setTagInput] = useState("");
+
+  const addTags = (tag) => {
+    setTags((pre) => {
+      return [...pre, tag];
+    });
+    setTagInput("");
+  };
+
+  const handleInputChange = (e) => {
+    setTagInput(e.target.value);
+  };
+
+  const handleInputKeyDown = (e) => {
+    if (e.key === "Enter" && tagInput.trim() !== "") {
+      addTags(tagInput);
+    }
+  };
+  const removeChip = (tagName, index) => {
+    setTags((pre)=>{
+      let allTags = [...tags]
+      allTags.splice(index,1)
+      return allTags
+    })
+  };
   return (
     <div className={`modalWrapper ${isOpen ? "open" : "close"}`}>
       <div className="modalBody">
@@ -13,11 +40,11 @@ const AddTaskModal = ({ isOpen, closeModal }) => {
           <CloseIcon className="icon" onClick={() => closeModal(false)} />
         </div>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" placeholder="title" />
+        <input id="title" type="text" placeholder="Title" />
         <label htmlFor="description">Description</label>
         <textarea
           name="description"
-          placeholder="description"
+          placeholder="Description"
           id="description"
           cols="37"
           rows="4"
@@ -31,7 +58,18 @@ const AddTaskModal = ({ isOpen, closeModal }) => {
           <option value="low">Low</option>
         </select>
         <label htmlFor="Tags">Tags</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={tagInput}
+          onChange={handleInputChange}
+          onKeyDown={handleInputKeyDown}
+          placeholder="Type and press Enter to add tags"
+        />
+        <div className="tags">
+          {tags.map((tag, index) => (
+            <Chip key={index} tag={tag} index={index} remove={removeChip} />
+          ))}
+        </div>
         <button className="create">Create</button>
       </div>
     </div>
