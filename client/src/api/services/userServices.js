26ef8735bbf56a72781aeb07bfa94a55f79866f3
rishianand39 <rishi.rn818@gmail.com>
2023-11-12 22:50:04 +0530

    redux tookit setup and signin handle

---
+++ client/src/api/services/userServices.js
@@ -0,0 +1,23 @@
+import { API_GATEWAY_BASE_URL } from "../config/axiosConfig";
+
+export const handleSignIn = async (userInfo) => {
+  try {
+    const response = await fetch(`${API_GATEWAY_BASE_URL}/user/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: userInfo.email,
+        password: userInfo.password,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message);
+    }
+    const userData = await response.json();
+    return userData;
+  } catch (error) {}
+};
