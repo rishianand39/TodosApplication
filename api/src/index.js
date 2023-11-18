565b197ecc4f713eb6c0ea6a91e5bff3ea4c901f
rishianand39 <rishi.rn818@gmail.com>
2023-11-18 21:13:58 +0530

    frontend application port enabled

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const { ConnectWithDataBase } = require("./database/dbConnect");
+const cors = require('cors');
 const app = express();
 ConnectWithDataBase()
 require("dotenv").config();
@@ -10,7 +11,17 @@ const taskController = require("./controller/task.controller")
 
 
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://task-manager-ra.vercel.app',
+];
+
 //MIDDLEWARES
+app.use(cors({
+  origin: allowedOrigins,
+  optionsSuccessStatus: 200,
+}));
+app.use(cors());
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
