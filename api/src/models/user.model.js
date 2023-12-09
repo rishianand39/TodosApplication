47b6e936a9960fc673b390becd5812f6ba2a5b41
rishianand39 <rishi.rn818@gmail.com>
2023-12-09 23:07:31 +0530

    user model modified

--- api/src/models/user.model.js
+++ api/src/models/user.model.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose')
 const UserSchema = mongoose.Schema({
     name : {type : String , reqired : true,},
     avatar : {type : String, required : false},
+    avatarColor : {type : String, required : false},
+    coverImage : {type : String, required : false},
+    coverColor : {type : String, required : false},
+    country : {type : String, required : false},
+    city : {type : String, required : false},
+    phone : {type : Number, required : false, maxLength:10},
     email : {type : String, required : true,},
     password  : {type  : String, required :true, minLength : 6, maxLength : 20}
 },{
