658ea2cfab200094a48cb4137f33672966639fd7
rishianand39 <rishi.rn818@gmail.com>
2023-12-11 21:52:01 +0530

    event handler made for profile update

--- client/src/building-block/profileImage.jsx
+++ client/src/building-block/profileImage.jsx
@@ -3,14 +3,14 @@ import {ProfileAvatar} from "../styles/styled-components/container"
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 
 
-const ProfileImage = () => {
+const ProfileImage = ({handleImageUpload}) => {
   return (
     <ProfileAvatar>
       <div className='cameraIconContainer'>
       <label htmlFor="file">
         <CameraAltIcon className='icon' />
         </label>
-        <input type="file" id='file' />
+        <input type="file" id='file' name="avatar" onChange={handleImageUpload}/>
       </div>
     </ProfileAvatar>
   )
