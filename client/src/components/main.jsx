96e99fd28411caf7ec59ffaef51b13df389e3d22
rishianand39 <rishi.rn818@gmail.com>
2024-01-01 23:10:06 +0530

    task category developement completed

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -5,7 +5,7 @@ import AddIcon from "@mui/icons-material/Add";
 import Tabs from "./tabs";
 import TaskCard from "./taskCard";
 import AddTaskModal from "./addTaskModal";
-import { NavLink, useLocation, useSearchParams } from "react-router-dom";
+import {useLocation, useSearchParams } from "react-router-dom";
 import { fetchTasks } from "../api/services/taskServices";
 import { useDispatch } from "react-redux";
 import { setMessage } from "../redux/notificationSlice";
@@ -17,6 +17,12 @@ const Main = () => {
   const location = useLocation();
   const dispatch = useDispatch();
   const [page, setPage] = useState(1);
+  const [taskCategory, setTaskCategory] = useState({
+    "inProgress" : 0,
+    "onHold" : 0,
+    "newAssigned" : 0,
+    "completed" : 0,
+  })
   let [searchParams, setSearchParams] = useSearchParams();
   const handleChange = (value) => {
     setSearchParams({ page: value });
@@ -27,13 +33,24 @@ const Main = () => {
     location.search == "" && setPage(1);
   }, [location.search]);
   
-  console.log(tasks, "taskd")
   useEffect(() => {
     (async function fetchAllTasks() {
       try {
         let query = location?.search == "" ? "?page=1" : location?.search;
         let tasks = await fetchTasks(query);
+        let inProgress, newAssigned, completed, onHold;
         if (tasks?.ok) { 
+          inProgress = tasks?.data?.filter(task=> task?.work_status == "In Progress")
+          onHold = tasks?.data?.filter(task=> task?.work_status == "On Hold")
+          completed = tasks?.data?.filter(task=> task?.work_status == "Completed")
+          setTaskCategory(pre=>{
+            return{
+              ...pre,
+              "inProgress": inProgress?.length,
+              "onHold": onHold?.length,
+              "completed": completed?.length,
+            }
+          })
           setTasks(tasks?.data);
         } else {
           dispatch(
@@ -41,27 +58,28 @@ const Main = () => {
               notificationType: "error",
               message: tasks?.message,
             })
-          );
-        }
+            );
+          }
       } catch (error) {
         dispatch(
           setMessage({
             notificationType: "error",
             message: error?.message,
           })
-        );
-      }
-    })();
-  }, [openTaskModal, searchParams]);
+          );
+        }
+      })();
+    }, [openTaskModal, searchParams]);
+    
 
-console.log(tasks, "tasks")
   return (
     <div className="main">
       <div className="left">
         <div className="cardContainer">
-          <Card title="Task In Progress" bg={"#f48942"} />
-          <Card title="New Assigned" bg={"#817cdf"} />
-          <Card title="Task completed" bg={"#38b7d3"} />
+          <Card value ={taskCategory?.inProgress} title="Task In Progress" bg={"#f48942"} />
+          <Card value ={taskCategory?.newAssigned} title="New Assigned" bg={"#817cdf"} />
+          <Card value ={taskCategory?.completed} title="Task completed" bg={"#38b7d3"} />
+          <Card value ={taskCategory?.onHold} title="Task On Hold" bg={"#e54b6e"} />
         </div>
         <div className="tasks">
           <div className="title">My Tasks</div>
